a3bd2b5c1a6b42b117ef9fbd05ca7a1ff1e926e4
NicolasCoge <chaosir@MacMini.com>
2021-06-20 00:44:04 +0800

    chaosir

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <el-button type="primary">冒泡排序</el-button>
+    <el-button type="success">插入排序</el-button>
+    <el-button type="info">选择排序</el-button>
+    <el-button type="warning">归并排序</el-button>
+    <el-button type="danger">快速排序</el-button>
+  </Layout>
+</template>
+<script>
+import Layout from "@/components/Layout";
+export default {
+  name: 'Home',
+  components: {Layout}
+}
+</script>
